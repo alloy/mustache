60e257e8de717377675774ae766bf7bfbe7a8ad3
Chris Wanstrath <chris@ozmm.org>
2009-11-25 02:53:37 -0800

    clean up implicit iterator pragma

--- lib/mustache/pragmas/implicit_iterator.rb
+++ lib/mustache/pragmas/implicit_iterator.rb
@@ -1,14 +1,22 @@
+class Mustache
+  module Pragmas
+    module ImplicitIterator
+      attr_accessor :iterator
+
+      def [](name)
+        el = super
+        if el.is_a?(Array)
+          el.map { |e| !e.is_a?(Hash) ? { iterator => e } : e }
+        else
+          el
+        end
+      end
+    end
+  end
+end
+
 Mustache::PRAGMAS['IMPLICIT-ITERATOR'] = proc do |options|
   iterator = options[:iterator] || '.'
-
-<<-ruby
-\#{def ctx.[](name)
-  el = super
-  if el.is_a?(Array)
-    el.map { |e| !e.is_a?(Hash) ? { '#{iterator}' => e } : e }
-  else
-    el
-  end
-end}
-ruby
+  '#{(class<<ctx;self;end).send(:include,%s);%s;nil}' %
+    [ 'Mustache::Pragmas::ImplicitIterator', "ctx.iterator='#{iterator}'" ]
 end
--- test/pragma_test.rb
+++ test/pragma_test.rb
@@ -5,20 +5,24 @@ class PragmaTest < Test::Unit::TestCase
   def test_implicit_iterator
     assert_equal <<-text, Pragma.render
 
+* Chris
+* Mark
+* Scott
 
 * Chris
 * Mark
 * Scott
 
-
 * Chris
 * Mark
 * Scott
+text
+  end
 
-
+  def test_dot_notation
+    assert_equal <<-text, PragmaDotNotation.render
 * Chris
-* Mark
-* Scott
+* 24
 text
   end
 end
